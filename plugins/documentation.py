841003eab17de51a222fc17e6156cf3eb4fc57a3
Andrey Cherepanov <cas@altlinux.org>
2025-01-24 11:51:14 +0300

    documentation: remove default font (by ShevSergey)

--- plugins/documentation.py
+++ plugins/documentation.py
@@ -24,7 +24,6 @@ class PluginDocumentation2(plugins.Base):
 
         self.text_browser = QTextBrowser()
         self.text_browser.setOpenExternalLinks(True)
-        self.text_browser.setCurrentFont(QFont("monospace", 9))
         self.index = pane.addWidget(self.text_browser)
 
         current_file = os.path.abspath(__file__)
